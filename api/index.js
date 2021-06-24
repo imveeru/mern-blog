a6930b70cdc52d738f63ac90c50c29cd28b1ee60
imvee <vind.veeru@gmail.com>
2021-06-24 20:25:49 +0530

    Configured dotEnv file

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app=express();
+const dotEnv=require('dotenv')
+const mongoose=require ('mongoose')
+
+dotEnv.config()
+
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use('/',(req,res)=>{
     console.log('Hey')
 })
+
 app.listen('5000',()=>{
     console.log('Backend is running')
-})
+});
