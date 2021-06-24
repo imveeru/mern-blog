b082ef603eb1d7945c58781b4ba9b80de3c50730
imvee <vind.veeru@gmail.com>
2021-06-24 23:11:45 +0530

    Added login feature in api

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -21,4 +21,22 @@ router.post('/register',async(req,res)=>{
     }
 } )
 
+
+//login
+
+router.post('/login',async(req,res)=>{
+    try{
+      const user = await User.findOne({username:req.body.username})
+      !user&& res.status(400).json('User not found!')
+
+      const validated = await bcrypt.compare(req.body.password,user.password)
+      !validated&& res.status(400).json('Wrong password')
+
+      res.status(200).json(user)
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
 module.exports=router;
