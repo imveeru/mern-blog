5a956e35ce69442a5bc9e4d5190e20e345d3fbe1
imvee <vind.veeru@gmail.com>
2021-06-24 21:57:14 +0530

    Made some minor tweaks.

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app=express();
-const dotEnv=require('dotenv')
+const dotenv=require('dotenv')
 const mongoose=require ('mongoose')
 
-dotEnv.config()
+dotenv.config(); // to access mongo url
 
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex:true
+}).then(
+    console.log('Connected to MongoDB')
+).catch(err=>{
+    console.log(err)
+})
 
 app.use('/',(req,res)=>{
     console.log('Hey')
