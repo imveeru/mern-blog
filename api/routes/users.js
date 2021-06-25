3672177152b6a542f15115ad0ba2233888d36b2e
imvee <vind.veeru@gmail.com>
2021-06-25 20:00:00 +0530

    Added delete user route.

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router=require('express').Router();
 const User=require('../models/User');
+const Post= require('../models/Post')
 const bcrypt = require('bcrypt');
 
 //update
@@ -17,7 +18,7 @@ router.put('/:id',async(req,res)=>{
             },{new:true}); // send the updated user as response
             res.status(200).json(updatedUser)
         } catch(err){
-            res.status.json(err)
+            res.status(500).json(err)
         }
     }else{
         res.status(401).json('You can only update your account!');
@@ -26,5 +27,24 @@ router.put('/:id',async(req,res)=>{
 });
 
 //delete
+
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id) {
+      try {
+        const user = await User.findById(req.params.id);
+        try {
+          await Post.deleteMany({ username: user.username });
+          await User.findByIdAndDelete(req.params.id);
+          res.status(200).json("User has been deleted...");
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      } catch (err) {
+        res.status(404).json("User not found!");
+      }
+    } else {
+      res.status(401).json("You can delete only your account!");
+    }
+  });
 
 module.exports=router;
