e70d598445b1f1d9d73d4d9dd4b9fffec0e5d92c
imvee <vind.veeru@gmail.com>
2021-06-25 20:29:18 +0530

    Added route to get individual post

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -71,9 +71,8 @@ router.delete("/:id", async (req, res) => {
 
 router.get('/:id',async(req,res)=>{
     try{
-        const user = await User.findById(req.params.id)
-        const {password,...others}=user._doc
-        res.status(200).json(others)
+        const post = await Post.findById(req.params.id)
+        res.status(200).json(post)
 
     } catch(err){
         res.status(500).json(err)
