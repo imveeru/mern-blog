572df49d9fe5044e1da86969f0711f4bd9bc20d4
imvee <vind.veeru@gmail.com>
2021-06-25 20:06:50 +0530

    Added get user route.

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -17,7 +17,7 @@ router.post('/register',async(req,res)=>{
         const user = await newUser.save();
         res.status(200).json(user)
     } catch(err){
-        res.status.json(err)
+        res.status(500).json(err)
     }
 } )
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -47,4 +47,17 @@ router.delete("/:id", async (req, res) => {
     }
   });
 
+// get
+
+router.get('/:id',async(req,res)=>{
+    try{
+        const user = await User.findById(req.params.id)
+        const {password,...others}=user._doc
+        res.status(200).json(others)
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
 module.exports=router;
