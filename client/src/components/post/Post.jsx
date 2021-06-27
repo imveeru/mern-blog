96263dffd178793920d356819072e45de69b34cf
imvee <vind.veeru@gmail.com>
2021-06-27 22:53:06 +0530

    :zap: Add links in posts

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './post.css'
+import {Link} from 'react-router-dom'
 
 function Post() {
     return (
@@ -11,10 +12,10 @@ function Post() {
             />
             <div className='postInfo'>
                 <div className='cats'>
-                    <span className='postCat'>Music</span>
-                    <span className='postCat'>Life</span>
+                    <Link to='/post/:catName' style={{textDecoration: 'none'}} className='postCat'>Music</Link>
+                    <Link to='/post/:catName' style={{textDecoration: 'none'}} className='postCat'>Life</Link>
                 </div>
-                <span className='postTitle'>Hello World!</span>
+                <Link to='/post/:postName' style={{textDecoration: 'none',color:'inherit'}} className='postTitle'>Hello World!</Link>
                 <hr/>
                 <span className='postDate'>1 hour ago</span>
                 <p className='postDesc'>Officia ipsum duis aute eiusmod ea exercitation elit non esse sint. Non minim ad occaecat proident cillum occaecat anim cillum do anim ullamco. Ea ex fugiat minim et pariatur proident commodo laborum qui. Aute elit duis irure laborum aliqua fugiat amet aliqua. Cillum proident ut proident pariatur ex magna eiusmod anim.Officia ipsum duis aute eiusmod ea exercitation elit non esse sint. Non minim ad occaecat proident cillum occaecat anim cillum do anim ullamco. Ea ex fugiat minim et pariatur proident commodo laborum qui. Aute elit duis irure laborum aliqua fugiat amet aliqua. Cillum proident ut proident pariatur ex magna eiusmod anim.Officia ipsum duis aute eiusmod ea exercitation elit non esse sint. Non minim ad occaecat proident cillum occaecat anim cillum do anim ullamco. Ea ex fugiat minim et pariatur proident commodo laborum qui. Aute elit duis irure laborum aliqua fugiat amet aliqua. Cillum proident ut proident pariatur ex magna eiusmod anim.Officia ipsum duis aute eiusmod ea exercitation elit non esse sint. Non minim ad occaecat proident cillum occaecat anim cillum do anim ullamco. Ea ex fugiat minim et pariatur proident commodo laborum qui. Aute elit duis irure laborum aliqua fugiat amet aliqua. Cillum proident ut proident pariatur ex magna eiusmod anim.Officia ipsum duis aute eiusmod ea exercitation elit non esse sint. Non minim ad occaecat proident cillum occaecat anim cillum do anim ullamco. Ea ex fugiat minim et pariatur proident commodo laborum qui. Aute elit duis irure laborum aliqua fugiat amet aliqua. Cillum proident ut proident pariatur ex magna eiusmod anim.Officia ipsum duis aute eiusmod ea exercitation elit non esse sint. Non minim ad occaecat proident cillum occaecat anim cillum do anim ullamco. Ea ex fugiat minim et pariatur proident commodo laborum qui. Aute elit duis irure laborum aliqua fugiat amet aliqua. Cillum proident ut proident pariatur ex magna eiusmod anim.</p>
