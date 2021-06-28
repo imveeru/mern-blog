bdbcdacb7a993ccc90a681b73b9fae507fb7e7f0
imvee <vind.veeru@gmail.com>
2021-06-28 20:16:08 +0530

    :zap: Added proxy to frontend.

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import './home.css'
 import Header from '../../components/header/Header'
 import Posts from '../../components/posts/Posts'
+import axios from 'axios'
 
 function Home() {
+
+    const [posts,setPosts]=useState([])
+
+    useEffect(()=>{
+        const fetchPosts=async ()=>{
+            axios.get("")
+        }
+    },[])
+
     return (
         <>
             <Header/>
