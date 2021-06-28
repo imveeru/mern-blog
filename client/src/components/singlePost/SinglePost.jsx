7ecc183929f44d7d4a311f54c61d9f81f762b05f
imvee <vind.veeru@gmail.com>
2021-06-28 21:11:12 +0530

    :zap: Fetched post data for single post page.

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,37 +1,49 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import {useLocation} from 'react-router'
 import './singlePost.css'
 import {GrEdit} from 'react-icons/gr'
 import {RiDeleteBin6Line} from 'react-icons/ri'
+import axios from 'axios'
 
 function SinglePost() {
 
-    
+    const location=useLocation()
+    const path=location.pathname.split('/')[2]
+    const [post,setPost]=useState([])
+
+    useEffect(() => {
+        const fetchPost = async()=>{
+            const res= await axios.get(`/posts/${path}`)
+            setPost(res.data)
+        }
+        fetchPost()
+    }, [path])
+
+    console.log(post)
 
     return (
         <div className="singlePost">
             <div className='singlePostWrapper'>
-                <img
+                {post.photo && (
+                    <img
                     className='singlePostImg'
-                    src='https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'
+                    src={post.photo}
                     alt='PostImg'
                 />
-                <h1 className='singlePostTitle'>Hello World!
+                )}
+                
+                <h1 className='singlePostTitle'>{post.title}
                     <div className='singlePostEdit'>
                         <GrEdit className='singlePostIcon'/>
                         <RiDeleteBin6Line className='singlePostIcon'/>
                     </div>
                 </h1>
                 <div className='singlePostInfo'>
-                    <span className='singlePostAuthor'>Author : <b>John Doe</b></span>
-                    <span className='singlePostDate'>1 hour ago</span>
+                    <span className='singlePostAuthor'>Author : <b>{post.username}</b></span>
+                    <span className='singlePostDate'>{new Date(post.createdAt).toDateString()}</span>
                 </div>
                 <p className='singlePostDesc'>
-                    Consectetur tempor consectetur non deserunt nulla veniam laborum. Mollit sit qui dolore ex anim ullamco nostrud elit irure irure. Amet cupidatat dolore occaecat minim velit nulla enim laborum consequat ipsum laborum cupidatat.                    Consectetur tempor consectetur non deserunt nulla veniam laborum. Mollit sit qui dolore ex anim ullamco nostrud elit irure irure. Amet cupidatat dolore occaecat minim velit nulla enim laborum consequat ipsum laborum cupidatat.
-                    Consectetur tempor consectetur non deserunt nulla veniam laborum. Mollit sit qui dolore ex anim ullamco nostrud elit irure irure. Amet cupidatat dolore occaecat minim velit nulla enim laborum consequat ipsum laborum cupidatat.
-                    Consectetur tempor consectetur non deserunt nulla veniam laborum. Mollit sit qui dolore ex anim ullamco nostrud elit irure irure. Amet cupidatat dolore occaecat minim velit nulla enim laborum consequat ipsum laborum cupidatat.
-                    Consectetur tempor consectetur non deserunt nulla veniam laborum. Mollit sit qui dolore ex anim ullamco nostrud elit irure irure. Amet cupidatat dolore occaecat minim velit nulla enim laborum consequat ipsum laborum cupidatat.
-                    Consectetur tempor consectetur non deserunt nulla veniam laborum. Mollit sit qui dolore ex anim ullamco nostrud elit irure irure. Amet cupidatat dolore occaecat minim velit nulla enim laborum consequat ipsum laborum cupidatat.
-                    Consectetur tempor consectetur non deserunt nulla veniam laborum. Mollit sit qui dolore ex anim ullamco nostrud elit irure irure. Amet cupidatat dolore occaecat minim velit nulla enim laborum consequat ipsum laborum cupidatat.
+                    {post.desc}
                 </p>
             </div>
         </div>
