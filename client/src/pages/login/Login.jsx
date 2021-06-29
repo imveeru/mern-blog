6041692a34ffca001d478a5a212208a5d28d080d
imvee <vind.veeru@gmail.com>
2021-06-29 21:35:45 +0530

    :zap: Added user Login request.

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,16 +1,32 @@
-import React,{useRef} from 'react'
+import React,{useRef,useContext} from 'react'
 import './login.css'
 import {FaKey,FaUserAlt} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
+import {Context} from '../../context/Context'
+import axios from 'axios'
 
 function Login() {
 
     const userRef=useRef();
     const passwordRef=useRef();
+    const {user,dispatch,isFetching}=useContext(Context)
 
-    const handleSubmit=(e)=>{
+    const handleSubmit=async (e)=>{
         e.preventDefault();
+        dispatch({type:'LOGIN_START'})
+        try{
+            const res = await axios.post('/auth/login',{
+                username:userRef.current.value,
+                password:passwordRef.current.value
+            })
+            dispatch({type:'LOGIN_SUCCESS',payload:res.data})
+        }catch(err){
+            dispatch({type:'LOGIN_FAILURE'})
+        }   
     }
+
+    console.log(user)
+
     return (
         <div className="login">
             <div className='loginHeader'>
