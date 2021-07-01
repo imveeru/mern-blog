b60d35e172874047bab7e6a13b0e40b2f7b4287a
imvee <vind.veeru@gmail.com>
2021-07-01 20:29:15 +0530

    :zap: Added delete post feature.

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,10 +1,11 @@
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useState,useContext} from 'react'
 import {useLocation} from 'react-router'
 import './singlePost.css'
 import {GrEdit} from 'react-icons/gr'
 import {RiDeleteBin6Line} from 'react-icons/ri'
 import axios from 'axios'
 import {Link} from 'react-router-dom'
+import {Context} from '../../context/Context'
 
 function SinglePost() {
 
@@ -24,6 +25,17 @@ function SinglePost() {
 
     console.log(post)
 
+    const {user} = useContext(Context)
+
+    const handleDelete=async ()=>{
+        try{
+            await axios.delete(`/posts/${path}`,{
+                data:{username:user.username} // data cannot be passed directly
+            })
+            window.location.replace('/')
+        }catch(err){}
+    }
+
     return (
         <div className="singlePost">
             <div className='singlePostWrapper'>
@@ -36,10 +48,12 @@ function SinglePost() {
                 )}
                 
                 <h1 className='singlePostTitle'>{post.title}
+                    {post.username===user?.username&&
                     <div className='singlePostEdit'>
                         <GrEdit className='singlePostIcon'/>
-                        <RiDeleteBin6Line className='singlePostIcon'/>
+                        <RiDeleteBin6Line className='singlePostIcon' onClick={handleDelete}/>
                     </div>
+                    }
                 </h1>
                 <div className='singlePostInfo'>
                     <span className='singlePostAuthor'>Author : <Link to={`/?user=${post.username}`} style={{textDecoration: 'none',color: 'inherit'}}><b>{post.username}</b></Link></span>
