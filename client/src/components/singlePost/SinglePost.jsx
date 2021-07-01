6fcfe7a1d64c720efca212e396c77c0a77dc06de
imvee <vind.veeru@gmail.com>
2021-07-01 21:07:56 +0530

    :zap: Added Update post feature.

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -15,10 +15,16 @@ function SinglePost() {
     const path=location.pathname.split('/')[2]
     const [post,setPost]=useState([])
 
+    const[title,setTitle]=useState('')
+    const[desc,setDesc]=useState('')
+    const[updateMode,setUpdateMode]=useState(false)
+
     useEffect(() => {
         const fetchPost = async()=>{
             const res= await axios.get(`/posts/${path}`)
             setPost(res.data)
+            setTitle(res.data.title)
+            setDesc(res.data.desc)
         }
         fetchPost()
     }, [path])
@@ -36,6 +42,16 @@ function SinglePost() {
         }catch(err){}
     }
 
+    const handleUpdate=async ()=>{
+        try{
+            await axios.put(`/posts/${path}`,{
+                username:user.username,title,desc // data can be passed directly
+            })
+            setUpdateMode(false)
+            // window.location.reload('/')
+        }catch(err){}
+    }
+
     return (
         <div className="singlePost">
             <div className='singlePostWrapper'>
@@ -47,21 +63,31 @@ function SinglePost() {
                 />
                 )}
                 
-                <h1 className='singlePostTitle'>{post.title}
-                    {post.username===user?.username&&
-                    <div className='singlePostEdit'>
-                        <GrEdit className='singlePostIcon'/>
-                        <RiDeleteBin6Line className='singlePostIcon' onClick={handleDelete}/>
-                    </div>
-                    }
-                </h1>
+                {updateMode?<input type='text' value={title} className='singlePostTitleInput' autoFocus onChange={e=>setTitle(e.target.value)}></input>: 
+                    (<h1 className='singlePostTitle'>{title}
+                        {post.username===user?.username&&
+                        <div className='singlePostEdit'>
+                            <GrEdit className='singlePostIcon' onClick={()=>setUpdateMode(true)}/>
+                            <RiDeleteBin6Line className='singlePostIcon' onClick={handleDelete}/>
+                        </div>
+                        }
+                    </h1>)
+                }
                 <div className='singlePostInfo'>
                     <span className='singlePostAuthor'>Author : <Link to={`/?user=${post.username}`} style={{textDecoration: 'none',color: 'inherit'}}><b>{post.username}</b></Link></span>
                     <span className='singlePostDate'>{new Date(post.createdAt).toDateString()}</span>
                 </div>
-                <p className='singlePostDesc'>
-                    {post.desc}
-                </p>
+
+                {updateMode?<textarea value={desc} className='singlePostDescInput' onChange={e=>setDesc(e.target.value)}></textarea>:
+                    (<p className='singlePostDesc'>
+                        {desc}
+                    </p>)
+                }
+
+                {updateMode&&
+                <button className='singlePostBtn' onClick={handleUpdate}>Update</button>
+                }
+
             </div>
         </div>
     )
