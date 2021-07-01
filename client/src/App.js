4a64cfc8a6f05188842fbcbf391a792b4ab3b8a5
imvee <vind.veeru@gmail.com>
2021-07-02 00:36:16 +0530

    :sparkles: Added a footer.

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Write from './pages/write/Write'
 import Settings from './pages/settings/Settings'
 import Login from './pages/login/Login'
 import Register from './pages/register/Register'
+import Footer from './components/footer/Footer'
+import './App.css'
 import {
   BrowserRouter as Router,
   Switch,
@@ -21,26 +23,29 @@ function App() {
     <Router>
       <div className="App">
         <NavBar/>
-          <Switch>
-              <Route exact path='/'>
-                <Home/>
-              </Route>
-              <Route path='/register'>
-                {user?<Home/>:<Register/>}
-              </Route>
-              <Route path='/login'>
-                {user?<Home/>:<Login/>}
-              </Route>
-              <Route path='/write'>
-                {user?<Write/>:<Register/>}
-              </Route>
-              <Route path='/settings'>
-                {user?<Settings/>:<Register/>}
-              </Route>
-              <Route path='/post/:postid'>
-                <Single/>
-              </Route>
-          </Switch>
+         <div className='appContainer'>
+            <Switch>
+                <Route exact path='/'>
+                  <Home/>
+                </Route>
+                <Route path='/register'>
+                  {user?<Home/>:<Register/>}
+                </Route>
+                <Route path='/login'>
+                  {user?<Home/>:<Login/>}
+                </Route>
+                <Route path='/write'>
+                  {user?<Write/>:<Register/>}
+                </Route>
+                <Route path='/settings'>
+                  {user?<Settings/>:<Register/>}
+                </Route>
+                <Route path='/post/:postid'>
+                  <Single/>
+                </Route>
+            </Switch>
+          </div>
+        {/* <Footer className='footer'/>   */}
       </div>
     </Router>
   );
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <span className='footerTxt'>Made with 💖 by <a href='https://github.com/imveeru/' style={{textDecoration: 'none',color:'inherit'}}>Veeramanohar</a></span>
+        </div>
+    )
+}
+
+export default Footer
