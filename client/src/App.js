1b5ac97db2a5df82c5bf1e77acdf8d2a8cb4a126
imvee <vind.veeru@gmail.com>
2021-07-03 19:11:39 +0530

    :zap: Fixed responsive forms.

--- client/src/App.js
+++ client/src/App.js
@@ -45,7 +45,7 @@ function App() {
                 </Route>
             </Switch>
           </div>
-        {/* <Footer className='footer'/>   */}
+        <Footer className='footer'/>  
       </div>
     </Router>
   );
