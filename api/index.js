b193204110a76112109a17aaad3f4fbac7949000
imvee <vind.veeru@gmail.com>
2021-07-03 21:45:44 +0530

    Made some changes.

--- api/index.js
+++ api/index.js
@@ -51,7 +51,7 @@ app.use('/api/categories',categoryRoute)
 
 //for heroku
 if(process.env.NODE_ENV=='production'){
-    app.use(express.static('../client/build'));
+    app.use(express.static('client/build'));
 
     app.get('*',(req,res)=>{
         res.sendFile(path.join(__dirname,'client','build','index.html'));//relative path
